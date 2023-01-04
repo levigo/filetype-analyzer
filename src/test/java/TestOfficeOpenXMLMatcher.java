7457d1f459997a2fc77eaece1926051961ca867f
Simon Welsch <s.welsch@levigo.de>
2023-01-04 17:30:07 +0100

    feat(JS-2357): detect MSVisio 2013 documents

--- src/test/java/TestOfficeOpenXMLMatcher.java
+++ src/test/java/TestOfficeOpenXMLMatcher.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.stream.Stream;
 
 import org.jadice.filetype.Context;
 import org.jadice.filetype.database.DescriptionAction;
@@ -15,6 +16,9 @@ import org.jadice.filetype.matchers.OfficeOpenXMLMatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class TestOfficeOpenXMLMatcher {
 
@@ -35,7 +39,7 @@ class TestOfficeOpenXMLMatcher {
   }
 
   @Test
-  public void testPPTXMatching() throws Exception {
+  void testPPTXMatching() throws Exception {
     final Context ctx = createContext("PowerPoint2007.pptx", false);
     final boolean matches = MATCHER.matches(ctx);
 
@@ -48,7 +52,7 @@ class TestOfficeOpenXMLMatcher {
   }
 
   @Test
-  public void testPPTXMatchingWithFileName() throws Exception {
+  void testPPTXMatchingWithFileName() throws Exception {
     final Context ctx = createContext("PowerPoint2007.pptx", true);
     final boolean matches = MATCHER.matches(ctx);
 
@@ -60,12 +64,52 @@ class TestOfficeOpenXMLMatcher {
 
   }
 
+  private static Stream<Arguments> provideTestData() {
+    return Stream.of(
+        Arguments.of("Visio_drawing.vsdx", "vsdx", "application/vnd.ms-visio.drawing", "Microsoft Visio 2013 Drawing"),
+        Arguments.of("Visio_drawing_macro-enabled.vsdm", "vsdm", "application/vnd.ms-visio.drawing.macroEnabled.12",
+            "Microsoft Visio 2013 macro-enabled Drawing"),
+        Arguments.of("Visio_stencil.vssx", "vssx", "application/vnd.ms-visio.stencil", "Microsoft Visio 2013 Stencil"),
+        Arguments.of("Visio_stencil_macro-enabled.vssm", "vssm", "application/vnd.ms-visio.stencil.macroEnabled.12",
+            "Microsoft Visio 2013 macro-enabled Stencil"),
+        Arguments.of("Visio_template.vstx", "vstx", "application/vnd.ms-visio.template",
+            "Microsoft Visio 2013 Template"),
+        Arguments.of("Visio_template_macro-enabled.vstm", "vstm", "application/vnd.ms-visio.template.macroEnabled.12",
+            "Microsoft Visio 2013 macro-enabled Template"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideTestData")
+  void testVSDXMatching(final String fileName, final String extension, final String mimeType, final String description) throws Exception {
+    final Context ctx = createContext(fileName, false);
+    final boolean matches = MATCHER.matches(ctx);
+
+    assertTrue(matches, "Matcher must match the given file");
+    assertEquals(extension, ctx.getProperty(ExtensionAction.KEY), "file extension");
+    assertEquals(mimeType, ctx.getProperty(MimeTypeAction.KEY), "MIME Type");
+    assertEquals(description, ctx.getProperty(DescriptionAction.KEY), "description");
+
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideTestData")
+  void testVSDXMatchingWithFileName(final String fileName, final String extension, final String mimeType, final String description) throws Exception {
+    final Context ctx = createContext(fileName, true);
+    final boolean matches = MATCHER.matches(ctx);
+
+    assertTrue(matches, "Matcher must match the given file");
+    assertEquals(extension, ctx.getProperty(ExtensionAction.KEY), "file extension");
+    assertEquals(mimeType, ctx.getProperty(MimeTypeAction.KEY), "MIME Type");
+    assertEquals(description, ctx.getProperty(DescriptionAction.KEY), "description");
+
+  }
+
   private static Context createContext(String fileName, boolean withFileName) throws IOException {
     final InputStream is = TestOfficeOpenXMLMatcher.class.getResourceAsStream("/various_types/" + fileName);
     if (withFileName) {
-      return new Context(new MemoryInputStream(is), new HashMap<String, Object>(), null, Locale.ENGLISH, fileName);
+      return new Context(new MemoryInputStream(is), new HashMap<>(), null, Locale.ENGLISH, fileName);
     } else {
-      return new Context(new MemoryInputStream(is), new HashMap<String, Object>(), null, Locale.ENGLISH, null);
+      return new Context(new MemoryInputStream(is), new HashMap<>(), null, Locale.ENGLISH, null);
     }
   }
 }
