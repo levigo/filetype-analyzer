c199b1afd40c422a4899db296d4fb21204b51ff2
David Werth <d.werth@levigo.de>
2025-05-23 11:23:45 +0200

    missing imports

--- src/main/java/org/jadice/filetype/matchers/PDFMatcher.java
+++ src/main/java/org/jadice/filetype/matchers/PDFMatcher.java
@@ -9,6 +9,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
 
